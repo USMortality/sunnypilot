51257f50c5e77aeb440177e923f3ad8be1ac09ee
USMortality.com <USMortality@protonmail.com>
2026-08-23 18:12:01 +0200

    Longitudinal: soften cruise acceleration ramp

--- openpilot/selfdrive/controls/lib/longitudinal_planner.py
+++ openpilot/selfdrive/controls/lib/longitudinal_planner.py
@@ -19,7 +19,7 @@ from openpilot.sunnypilot.selfdrive.controls.lib.longitudinal_planner import Lon
 
 A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6]
 A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
-J_CRUISE_VALS = [1.6, 1.2, 0.8, 0.6]
+J_CRUISE_VALS = [1.0, 0.85, 0.7, 0.6]
 A_CRUISE_MIN = -1.2
 CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 ALLOW_THROTTLE_THRESHOLD = 0.4
